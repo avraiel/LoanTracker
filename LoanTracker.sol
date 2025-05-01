7ff25953d948c0422c73ead3bd12f4f25bda251f
gab <gabriel.geraldo@student.ateneo.edu>
2025-05-01 15:31:47 +0800

    added contract

---
+++ LoanTracker.sol
@@ -0,0 +1,25 @@
+// DISCS Certificate of Authorship - Code Comment Block Certification
+// [Student Full Name]-[ID Number]-[Section]
+
+// Gabriel I. Geraldo-212734-MT
+
+// I hereby attest to the truth of the following facts:
+
+// I have not discussed the Solidity code in my program with anyone
+// other than my instructor or the teaching assistants assigned to this course.
+
+// I have not used Solidity code obtained from another student, or
+// any other unauthorized source, whether modified or unmodified.
+
+// If any Solidity code or documentation used in my program was
+// obtained from another source, it has been clearly noted with citations in the
+// comments of my program.
+
+
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
+
+
+public contract LoanTracker {
+
+}
